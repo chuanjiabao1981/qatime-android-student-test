7af41ce2d06ed42a232ccce48fd7a9fea47114b2
lungtify <15617685965@163.com>
2016-08-23 10:27:07 +0800

    登录测试

---
+++ appium/src/main/java/main/ConstantValue.java
@@ -0,0 +1,36 @@
+package main;
+
+/**
+ *
+ */
+public class ConstantValue {
+    /**
+     * app所在目录
+     */
+    public static String appPath = "C:\\use\\workplace\\qatime-android-student\\app\\build\\outputs\\apk";
+
+    /**
+     * app文件名
+     */
+    public static String appName = "app-debug.apk";
+
+    /**
+     * 设备名称
+     */
+    public static String deviceName = "HUAWEI KNT-UL10";
+
+    /**
+     * 版本号
+     */
+    public static String platformVersion = "6.0";
+
+    /**
+     * app包名
+     */
+    public static String appPackage = "cn.qatime.player";
+
+    /**
+     * 要启动的Activity
+     */
+    public static String appActivity = "activity.StartActivity";
+}
---
+++ appium/src/main/java/test/HomeTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import org.openqa.selenium.NoSuchElementException;
+
+import java.net.MalformedURLException;
+import java.util.List;
+
+import io.appium.java_client.android.AndroidElement;
+import main.AppiumTest;
+
+/**
+ * @author luntify
+ * @date 2016/8/22 12:20
+ * @Description 主页测试   mainactivity
+ */
+public class HomeTest extends AppiumTest {
+    @Override
+    protected void testStart() throws InterruptedException, MalformedURLException {
+
+    }
+
+    @Override
+    public void onHome() throws InterruptedException, NoSuchElementException, MalformedURLException {
+        super.onHome();
+
+        List<AndroidElement> lastnews = driver.findElementsById("tab_text1");//最新
+        List<AndroidElement> whole = driver.findElementsById("tab_text2");//全部
+
+        //最新下滑刷新
+        Time(2);
+        int width = driver.manage().window().getSize().width;
+        int height = driver.manage().window().getSize().height;
+        driver.swipe(width / 2, height, width / 2, height / 2, 200);
+        Time(2);
+        //全部下滑刷新
+        whole.get(0).click();//切换至全部页面
+        driver.swipe(width / 2, height, width / 2, height / 2, 200);
+        Time(2);
+        lastnews.get(0).click();
+
+    }
+}
