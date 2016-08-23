22f925a87a7d939cdcb630ac4c7ae2208e06aaee
139634zgc <13001253655@163.com>
2016-08-23 14:36:54 +0800

    辅导班tab测试

--- appium/src/main/java/test/MyTutorshipTest.java
+++ appium/src/main/java/test/MyTutorshipTest.java
@@ -21,7 +21,28 @@ public class MyTutorshipTest extends AppiumTest {
         AndroidElement calssed = driver.findElementById("calssed");
         calssed.click();
         Time(3);
-
-        
+        totalship();
     }
+
+
+    public void totalship() throws InterruptedException {
+        AndroidElement tabText1 = driver.findElementById("tab_text1");
+        tabText1.click();
+        Time(3);
+        AndroidElement tabText2 = driver.findElementById("tab_text2");
+        tabText2.click();
+        Time(3);
+        AndroidElement tabText5 = driver.findElementById("tab_text5");
+        tabText5.click();
+        Time(3);
+        AndroidElement tabText4 = driver.findElementById("tab_text4");
+        tabText4.click();
+        Time(3);
+        AndroidElement tabText3 = driver.findElementById("tab_text3");
+        tabText3.click();
+        Time(3);
+
+    }
+
+
 }
